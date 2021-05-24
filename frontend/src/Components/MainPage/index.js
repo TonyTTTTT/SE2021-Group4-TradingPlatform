e590b7070b6d6cd2c1005bf7f82219b3df599561
nick1889 <r07922151@ntu.edu.tw>
2021-05-24 22:08:47 +0800

    edit layout

--- frontend/src/Components/MainPage/index.js
+++ frontend/src/Components/MainPage/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Container,Row,Col,Button } from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 import Content from "./Content/content";
 import SideArea from "./SideArea/sideArea"
 import TagArea from "./TagArea/tagArea"
 import Console from "./Console/console"
+
 class MainPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
+
     render() {
         return (
-            <Container fluid>
+            <Container fluid style={{height:window.innerHeight,width:window.innerWidth}}>
                 <Row>
-                <Col>
-                    <Button variant="outline-dark"  href="/" block>Home</Button>
-                </Col>
+                    <Col>
+                        <Button variant="outline-dark" href="/" block>Home</Button>
+                    </Col>
                 </Row>
                 <Row>
                     <Col xs={4}>
@@ -24,12 +25,15 @@ class MainPage extends React.Component {
                         <TagArea/>
                     </Col>
                     <Col xs={8} style={{height: "100%"}}>
-						<Content/>
-						<Console/>
+                        <Container>
+                            <Content/>
+                            <Console/>
+                        </Container>
                     </Col>
                 </Row>
             </Container>
         );
     }
 }
+
 export default MainPage;
