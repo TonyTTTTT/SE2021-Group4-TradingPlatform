38eb31dfac6a8e8a19d433814fe518e6201e39f7
TonyTTTT <r09922176@csie.ntu.edu.tw>
2021-05-26 01:03:15 +0800

    edit main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, redirect, url_for, jsonify, send_from_directory
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
+cors = CORS(app)
 
 @app.route('/')
 def index():
@@ -16,3 +18,12 @@ def reports():
         return "Error: no name field provided."
 
     return send_from_directory('reports', name)
+
+
+@app.route('/get-all-report-list', method=['GET'])
+def get_all_report():
+    return
+
+@app.route('/get-report', method=['GET'])
+def get_report(report_id:int):
+    return
