3d734278a0efd7a44ebf9792590e21b460b60c12
nick1889 <r07922151@ntu.edu.tw>
2021-05-30 15:16:29 +0800

    Fix bug for that case that there is no report for an algo

--- backend/DataFileManager.py
+++ backend/DataFileManager.py
@@ -249,6 +249,8 @@ class DataFileManager(metaclass=Singleton):
             report_id = self._generate_report_id()
             report_info = ReportInfo(report_id, algo_id, title, str(report_path), time.asctime(time.localtime()))
             self.data[REPORT].append(report_info.__dict__)
+            if algo_id not in self.algo_id2report_ids:
+                self.algo_id2report_ids[algo_id] = []
             self.algo_id2report_ids[algo_id].append(report_id)
             self.report_id2report_info[report_id] = report_info.__dict__
             return report_id
