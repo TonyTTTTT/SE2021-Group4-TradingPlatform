c0239a8d0f50b155980d774433f4b713f8901d97
SE2021 Group4 <se2021_group4_leader@gmail.com>
2021-06-01 09:49:22 +0800

    Update gitlab-test.groovy

--- Jenkins/gitlab-test.groovy
+++ Jenkins/gitlab-test.groovy
@@ -8,7 +8,9 @@ pipeline {
         }
         state('Build') {
             steps {
-                sh 'pwd'
+                sh '''#!/bin/bash
+                pwd
+                '''
             }
         }
     }
