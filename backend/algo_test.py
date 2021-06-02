d76e25459d4cd141ff12a04c9541e03f357944ec
e23099 <e23099@gmail.com>
2021-06-02 14:45:48 +0800

    testing second_version.py for BuyAndHold

--- backend/algo_test.py
+++ backend/algo_test.py
@@ -1,12 +1,14 @@
-from algo_files.BuyAndHold.first_version import BH
+from algo_files.BuyAndHold._2 import BH
 from DataClasses import Parameter
 from Calculator import Calculator
 
 test = BH()
 test.set_parameter([
         Parameter("product", "cat", 'TXF'),
-        Parameter("long/short", "cat", 'long')
+        Parameter("long/short", "cat", 'long'),
+        Parameter("Days to maturity", "num", 0),
         ])
+test.set_product_date(start_date="2004-01-01", end_date="2100-01-01")
 ta = test.run()
 cal = Calculator()
 cal.set_slip(1)
