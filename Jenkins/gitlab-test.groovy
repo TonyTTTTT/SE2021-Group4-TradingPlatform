5681cdd9e2cc825c10c40d63b37f09b13fa790b8
TonyTTTT <r09922176@csie.ntu.edu.tw>
2021-06-03 17:50:05 +0800

    test for gitlab hook jenkins

--- Jenkins/gitlab-test.groovy
+++ Jenkins/gitlab-test.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Compile') {
             steps {
-                echo 'first stage, edit by Tony, v6' 
+                echo 'first stage, edit by Tony, v7' 
                 echo 'Echo test'
             }
         }
