aa0cb35639ef4c0d006ef5b5c95d61fb93e4bc03
e23099 <e23099@gmail.com>
2021-06-05 17:20:47 +0800

    jenkins test

--- Jenkins/pipeline.groovy
+++ Jenkins/pipeline.groovy
@@ -3,22 +3,22 @@ pipeline {
     stages {
         stage('Sync') {
             steps {
-                sh './Jenkins/script/sync.sh'
+                sh Jenkins/script/sync.sh
             }
         }
         stage('Install Dependency') {
             steps {
-                sh './Jenkins/script/install_dependency.sh'
+                sh Jenkins/script/install_dependency.sh
             }
         }
         stage('Build') {
             steps {
-                sh './Jenkins/script/build.sh'
+                sh Jenkins/script/build.sh
             }
         }
         stage('Test') {
             steps {
-                sh './Jenkins/script/test.sh'
+                sh Jenkins/script/test.sh
             }
         }
 	}
